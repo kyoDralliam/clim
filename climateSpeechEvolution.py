4ffef400e97d0711dd38bec93c359d82eac00c4c
Kenji Maillard <kenji@maillard.blue>
2025-03-22 15:15:45 +0100

    save climate related data

--- climateSpeechEvolution.py
+++ climateSpeechEvolution.py
@@ -4,6 +4,7 @@ from datetime import datetime, timezone
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from itertools import tee
 
 # Load all the speeches
 with open('speechs_data.json') as f:
@@ -16,20 +17,28 @@ kyoto_protocol = datetime(1997,12,12, tzinfo=timezone.utc)
 climate_words = r'climat|planéte|pollution|réchauffement|écolog|ZAD|zad'
 climate_regexp = re.compile(climate_words)
 
-# Process each entry to parse the datetime and adds an integer valued column `match` (containing 0 or 1)
+# Process each entry, return a new dictionnary with parsed datetime and adds an integer valued field `match` (containing 0 or 1)
 # indicating whether the entry has some references to climate change keywords in its text
 def process(x):
   d = datetime.fromisoformat(x['datetime'])
-  x['datetime'] = d
+  y = { 'datetime' : d }
   if kyoto_protocol < d and x['text']:
-    x['match'] = int(bool(5 < len (climate_regexp.findall(x['text']))))
+    y['match'] = int(bool(5 < len (climate_regexp.findall(x['text']))))
   else:
-    x['match'] = 0
-  return x
+    y['match'] = 0
+  return x, y
 
-# Apply the process function to all data, filter out entries before Kyoto's protocol (remove text as well)
-df = pd.DataFrame.from_records((x for x in map(process, after_kyoto) if kyoto_protocol < x['datetime']), index=['datetime'], exclude=['text'])
+# Apply the process function to all data, filter out entries before Kyoto's protocol
+speechs_after_kyoto = (x for x in map(process, speechs) if kyoto_protocol < x[1]['datetime'])
+# Split the generators in two, one for saving the data, the other for plotting
+speechs_after_kyoto1, speechs_after_kyoto2 = tee(speechs_after_kyoto)
 
+# Save the entries that refers to climate
+with open("climate.json", "w") as f:
+  json.dump(list(x[0] for x in speechs_after_kyoto1 if x[1]['match']), f, ensure_ascii=False,indent=2)
+
+# Transform to a panda dataframe removing all fields but 'match'
+df = pd.DataFrame.from_records((x[1] for x in speechs_after_kyoto2), index=['datetime'])
 
 # Group the data per quarter
 quarterly = df.groupby(pd.Grouper(freq="QE")).agg(
